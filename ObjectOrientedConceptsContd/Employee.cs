9db6e1eb2198ee33e060c9adc63da867cad16f14
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2023-10-05 14:06:31 +0530

    Add files via upload

---
+++ ObjectOrientedConceptsContd/Employee.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedConceptsContd
+{
+    public class Employee
+    {
+        //constructor---method with same name as that of class without any return type
+        //default constructor
+         public Employee() 
+        { }
+
+
+        int _empid;
+        string _ename;
+        string _loc;
+        //Parameterised constructor
+        public Employee(int empid,string name) 
+        { 
+            _empid = empid;
+            _ename = name;
+        
+
+        }
+
+
+        //Parameterised Constructor
+        public Employee(int empid,string name,string loc)
+        {
+            _empid=empid;
+            _ename = name;
+            _loc = loc;
+
+        }
+
+        //Read only property
+
+        public dynamic Details {
+            get {
+                string d = _empid.ToString() + _ename + _loc;
+                return d; }
+        }
+
+
+        public int Employeeid 
+        {
+            get
+            { 
+            return _empid;
+            }
+         }
+        public string EmpName
+        {
+            get { return _ename; }
+        }
+        public string Location
+        {
+            get { return _loc; }
+        
+        }
+
+
+    }
+}
---
+++ ObjectOrientedConceptsContd/constructorDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedConceptsContd
+{
+    internal class constructorDemo
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();//0&null
+            Employee emp1 = new Employee(12, "Gauri", "Bangalore");
+            Employee emp2 = new Employee(13, "Rajashri");
+            Employee emp3 = new Employee(empid: 78, loc: "TamilNadu", name: "Harish");
+            Console.WriteLine( emp1.Employeeid );
+            Console.WriteLine(emp1.EmpName);
+            Console.WriteLine(emp1.Location);
+            Console.WriteLine( "---------------");
+            Console.WriteLine(  emp3.Details);
+            Console.ReadLine();
+
+        }
+    }
+}
